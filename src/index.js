7ca605e6e08eda2c91d7e9b638ab59031fe40e4f
ybtole <btoleholtaquo@outlook.com>
2026-06-01 23:17:09 -0300

    Get funcionando

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ app.use(express.json());
 
 mongoose.connect(process.env.DB_MONGO);
 
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
 mongoose.connection.on("error", (error) => {
   console.error("Erro de conexão com o MongoDB:", error);
 });
 
 mongoose.connection.once("connected", () => {
-  console.log("Conectado ao MongoDB");
+  console.log(`Conectado ao MongoDB`);
 });
 
 app.get("/", (req, res) => {
-  res.send("Conectado MongoDB");
+  res.status(200).json({ message: "Conectado ao MongoDB" });
+  console.log("Get aqui");
 });
